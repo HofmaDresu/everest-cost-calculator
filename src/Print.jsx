22627c290242ead12fba5f58a13002a0ed40f0e1
Matt Hoffman <hofmadresu@gmail.com>
2017-09-29 15:39:31 -0400

    Populate gear items. Improve css

--- src/Print.jsx
+++ src/Print.jsx
@@ -12,21 +12,36 @@ export default function Print({appState}) {
                         (flights.includeVisaFees ? flights.visaCost * people.numberOfPeople : 0);
     const gearCost = gearItems.map(a => Object.assign({}, a)).reduce((sum, g) => sum + (g.price * g.currentCount), 0)
 
+    const gearLineItems = [];
+    for(let i = 0; i < gearItems.length; i++) {
+        let g = gearItems[i];
+        if(!g.currentCount) continue;
+        gearLineItems.push(<div>{`${g.currentCount} ${g.name}:`}</div>);
+        gearLineItems.push(<div>${g.price * g.currentCount}</div>);
+    }
+
     const totalCost = tripCost + flightCost + gearCost;
     return (
         <div className="print-section">
             <h1>Booking Costs</h1>
-            <div>Trip cost for {people.numberOfPeople} people:</div>
+            <div>Trip cost for {people.numberOfPeople}:</div>
             <div>${people.perPersonCost * people.numberOfPeople}</div>
-            {people.hasInsurance ? <div>Insurance cost for {people.numberOfPeople} people:</div> : null}
+            {people.hasInsurance ? <div>Insurance cost for {people.numberOfPeople}:</div> : null}
             {people.hasInsurance ? <div>${people.insuranceEachCost * people.numberOfPeople}</div> : null}   
             <div className="subtotal">Subtotal: </div>
             <div className="subtotal">${tripCost}</div>
             <h1>Travel Costs</h1>    
+            {flights.includeInternational ? <div>International flight cost for {people.numberOfPeople}:</div> : null}
+            {flights.includeInternational ? <div>${flights.internationalCost * people.numberOfPeople}</div> : null}   
+            {flights.includeDomesticInNepal ? <div>Domest flight cost for {people.numberOfPeople}:</div> : null}
+            {flights.includeDomesticInNepal ? <div>${flights.domesticCost * people.numberOfPeople}</div> : null}   
+            {flights.includeVisaFees ? <div>Visa fees for {people.numberOfPeople}:</div> : null}
+            {flights.includeVisaFees ? <div>${flights.visaCost * people.numberOfPeople}</div> : null}   
             <div className="subtotal">Subtotal: </div>
             <div className="subtotal">${flightCost}</div>
             
             <h1>Gear Costs</h1>   
+            {gearLineItems}
             <div className="subtotal">Subtotal: </div>
             <div className="subtotal">${gearCost}</div>
 
